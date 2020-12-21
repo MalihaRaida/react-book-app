fd91629247c92802909d92fb6ab9bf55a57ba6f7
Maliha Noushin Raida <malihanoushin@iut-dhaka.edu>
2020-12-21 12:57:59 +0600

    book description add on hover

--- src/components/BookCard.js
+++ src/components/BookCard.js
@@ -1,10 +1,14 @@
 import React from "react";
 import { BrowserRouter, Link, Route } from "react-router-dom";
+import "../BookCard.css";
 
 const BookCard = (props) => {
   const details = props.data.volumeInfo;
   let imageUrl = "https://picsum.photos/id/1040/200/300";
-  if (details.imageLinks != undefined && details.imageLinks.thumbnail !=undefined) {
+  if (
+    details.imageLinks != undefined &&
+    details.imageLinks.thumbnail != undefined
+  ) {
     imageUrl = details.imageLinks.thumbnail;
   }
   let author = " ";
@@ -12,9 +16,14 @@ const BookCard = (props) => {
     author = details.authors.join(", ");
   }
   const category = details.categories;
+  let description = "";
+  if (details.description != undefined && details.description.length > 200) {
+    description = details.description.substring(0, 200) + " ... ";
+  }
+
   return (
     <div className="column">
-      <div className="ui fluid card">
+      <div className="ui fluid card book-card">
         <div className="image">
           <img src={imageUrl} />
         </div>
@@ -36,6 +45,10 @@ const BookCard = (props) => {
             <i className="large star icon"></i>
           )}
           {details.averageRating}
+        </div>
+        <div className="book-menu">
+          <h3>Overview:</h3>
+          <p>{description}</p>
         </div>
       </div>
     </div>
