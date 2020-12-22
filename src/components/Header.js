eded7d0575dcde58d27720dc1c38773b34eff3b8
Maliha Noushin Raida <malihanoushin@iut-dhaka.edu>
2020-12-22 10:50:16 +0600

    Header reusable component

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,11 +1,11 @@
 import React from "react";
 
-const Header=()=>{
+const Header=(props)=>{
     return (
       <div className="ui inverted segment">
           <h2 className="ui inverted center aligned icon header">
             <i className="book icon"></i>
-            BookFinder
+            {props.title}
           </h2>
       </div>
     );
