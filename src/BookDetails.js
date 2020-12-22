7c2dfc27be212e0bdc8798582b446b1b29088d44
Maliha Noushin Raida <malihanoushin@iut-dhaka.edu>
2020-12-23 04:07:55 +0600

    BookDetails geting data

--- src/BookDetails.js
+++ src/BookDetails.js
@@ -1,20 +1,25 @@
 import React, { useEffect, useState } from "react";
 import Header from "./components/Header";
 import { getSingleBookById } from "./api/googleBook";
+import { useHistory } from "react-router-dom";
 
 const BookDetails = ({ match }) => {
   const BookId = match.params.id;
   const [book, setBook] = useState({});
+  const history=useHistory();
   const getBookDetails = async () => {
-    await getSingleBookById(BookId, setBook);
+    await getSingleBookById(BookId, setBook);  
   };
 
   useEffect(() => {
     getBookDetails();
-  });
+  }, []);
+
   return (
     <div className="book-result">
       <Header title="BookDetails" />
+      <p>{book.title}</p>
+      <button onClick={()=>history.goBack()}>Back</button>
     </div>
   );
 };
