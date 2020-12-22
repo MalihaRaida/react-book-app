e66167fb7eb6be1c683e62a12722209b616f17fa
Maliha Noushin Raida <malihanoushin@iut-dhaka.edu>
2020-12-22 11:32:13 +0600

    get book details

--- src/BookDetails.js
+++ src/BookDetails.js
@@ -1,13 +1,22 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import Header from "./components/Header";
+import { getSingleBookById } from "./api/googleBook";
 
-const BookDetails=({match})=>{
-    console.log(match.params.id)
-    return (
-      <div>
-        <Header title="BookDetails" />
-      </div>
-    );
-}
+const BookDetails = ({ match }) => {
+  const BookId = match.params.id;
+  const [book, setBook] = useState({});
+  const getBookDetails = async () => {
+    await getSingleBookById(BookId, setBook);
+  };
+
+  useEffect(() => {
+    getBookDetails();
+  });
+  return (
+    <div className="book-result">
+      <Header title="BookDetails" />
+    </div>
+  );
+};
 
 export default BookDetails;
